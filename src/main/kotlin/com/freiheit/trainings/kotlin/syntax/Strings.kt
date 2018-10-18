037a2dc71f47a21565553a910605cff8a225c130
Eugen Liebreich <eugen.liebreich@freiheit.com>
2018-10-18 14:17:36 +0200

    strings

--- src/main/kotlin/com/freiheit/trainings/kotlin/syntax/Strings.kt
+++ src/main/kotlin/com/freiheit/trainings/kotlin/syntax/Strings.kt
@@ -1,19 +1,36 @@
 package com.freiheit.trainings.kotlin.syntax
 
+/**
+ * Strings are beside primitive the only types eligible being compile-time constants
+ *
+ * @DO Reduce bytecode overhead by declaring static strings as constants
+ */
 private const val foo = "foo"
 
 object Bar {
     const val value = "bar"
 }
 
-
-// Substitution
+/**
+ * Kotlin understands string substitution à la JavaScript.
+ *
+ * @DO Use substitution instead of concatenation or String.format()
+ *
+ * Notice: Unlike JavaScript, no special quotes are requires. The usual double-quote supports substitution.
+ *         Single quotes are used to define special chars in names and to distinguish keywords from colliding function names.
+ */
 private const val substitution = "$foo bar"
+// use curlies, if there is no whitespace between the variable name and the rest of the string
 private const val trimmedSubstitution = "${foo}bar"
+// use curlies, if nested calls are required
 private const val substitutionNested = "$foo ${Bar.value}"
 
 
-// Multiline
+/**
+ * Kotlin supports Python like multiline strings.
+ *
+ * @DO Use multiline strings instead of \n for better readability
+ */
 private val multiline = """Fist line
     |next line
     |$foo
